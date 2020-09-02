53985a416cf168b6060dd616ffa9be6fc28d63c1
RonaldoMoraes <ronaldo.moraes1990@gmail.com>
2020-09-01 23:05:35 -0300

    #7 Sending email with gmail mandrill & mailgun

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
